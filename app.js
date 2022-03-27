22918f5654f1380c6e6877e319412d9fd23ea3ba
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-03-28 00:41:31 +0800

    Added

--- app.js
+++ app.js
@@ -13,17 +13,26 @@ const app = new App({
   socketMode: true,
 });
 
-app.message("muzo", async ({ say }) => {
+app.message("hello", async ({ message, say }) => {
   await say({
     blocks: [
       {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: ":muzo:",
+          text: `Hey there <@${message.user}>!`,
+        },
+        accessory: {
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: "Click Me",
+          },
+          action_id: "button_click",
         },
       },
     ],
+    text: `Hey there <@${message.user}>!`,
   });
 });
 
