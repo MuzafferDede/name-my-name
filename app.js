579e9f67b0d4910f4ccc32f93feb47cad3ab3662
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-03-27 23:45:12 +0800

    Added

--- app.js
+++ app.js
@@ -9,14 +9,11 @@ see: https://slack.dev/bolt-js/tutorial/getting-started
 // Initializes your app with your bot token and app token
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
 });
-console.log({
-  token: process.env.SLACK_BOT_TOKEN,
-  socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN,
-});
+
 // Listens to incoming messages that contain "hello"
 app.message("hello", async ({ message, say }) => {
   console.log("saying hi");
