6468ba6db44a5a0f7d60e60b45fedb719272d944
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-03-27 23:56:49 +0800

    Added

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.action("button_click", async ({ body, ack, say }) => {
 
 (async () => {
   // Start your app
-  await app.start(process.env.PORT || 3000);
+  await app.start();
 
   console.log("⚡️ Bolt app is running!");
 })();
