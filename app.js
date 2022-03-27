f3bbd1849d8c2c485f655ccc1f67ef15e5701578
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-03-27 23:38:07 +0800

    Added

--- app.js
+++ app.js
@@ -13,7 +13,12 @@ const app = new App({
   appToken: process.env.SLACK_APP_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
-
+console.log({
+  token: process.env.SLACK_BOT_TOKEN,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
 // Listens to incoming messages that contain "hello"
 app.message("hello", async ({ message, say }) => {
   console.log("saying hi");
