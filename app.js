2482a201c621c30cf0add18702ec39257346188f
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-03-29 02:24:28 +0800

    Added

--- app.js
+++ app.js
@@ -313,21 +313,21 @@ app.view("create_new_product_action", async ({ ack, view, client }) => {
           type: "section",
           text: {
             type: "plain_text",
-            text: `Product: ${view.state.values.product.product_select_action.selected_option.text}`,
+            text: `Product: ${view.state.values.product.product_select_action.selected_option.text.text}`,
           },
         },
         {
           type: "section",
           text: {
             type: "plain_text",
-            text: `Project: ${view.state.values.project.project_select_action.selected_option.text}`,
+            text: `Project: ${view.state.values.project.project_select_action.selected_option.text.text}`,
           },
         },
         {
           type: "section",
           text: {
             type: "plain_text",
-            text: `Role: ${view.state.values.role.role_select_action.selected_option.text}`,
+            text: `Role: ${view.state.values.role.role_select_action.selected_option.text.text}`,
           },
         },
         {
