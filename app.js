851a65ef09f06b36549ffe30b267d9f9d1073cbb
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-03-29 02:52:43 +0800

    Added

--- app.js
+++ app.js
@@ -306,60 +306,42 @@ app.view("create_new_product_action", async ({ ack, view, client }) => {
       blocks: [
         {
           type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `*Product*: ${view.state.values.product.product_select_action.selected_option.text.text}`,
-          },
-        },
-        {
-          type: "divider",
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `*Project*: ${view.state.values.project.project_select_action.selected_option.text.text}`,
-          },
-        },
-        {
-          type: "divider",
+          fields: [
+            {
+              type: "mrkdwn",
+              text: `*Product:*\n${view.state.values.product.product_select_action.selected_option.text.text}`,
+            },
+            {
+              type: "mrkdwn",
+              text: `*Project:*\n${view.state.values.project.project_select_action.selected_option.text.text}`,
+            },
+          ],
         },
         {
           type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `*Role*: ${view.state.values.role.role_select_action.selected_option.text.text}`,
-          },
-        },
-        {
-          type: "divider",
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `*Foundable's name*: ${view.state.values.foundable.foundable_action.value}`,
-          },
-        },
-        {
-          type: "divider",
+          fields: [
+            {
+              type: "mrkdwn",
+              text: `*Role:*\n${view.state.values.role.role_select_action.selected_option.text.text}`,
+            },
+            {
+              type: "mrkdwn",
+              text: `*Foundable's name:*\n${view.state.values.foundable.foundable_action.value}`,
+            },
+          ],
         },
         {
           type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `*Tag*: ${view.state.values.tag.tag_action.value}`,
-          },
-        },
-        {
-          type: "divider",
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `*URL*:  <${view.state.values.url.url_action.value}|${view.state.values.url.url_action.value}>`,
-          },
+          fields: [
+            {
+              type: "mrkdwn",
+              text: `*Tag:*\n${view.state.values.tag.tag_action.value}`,
+            },
+            {
+              type: "mrkdwn",
+              text: `*URL:*\n<${view.state.values.url.url_action.value}>|${view.state.values.url.url_action.value}`,
+            },
+          ],
         },
       ],
     },
