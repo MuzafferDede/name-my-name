d9f70ac274e393bed033ebbab1287a5c9e1d5e42
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-03-29 02:58:38 +0800

    Added

--- app.js
+++ app.js
@@ -309,27 +309,11 @@ app.view("create_new_product_action", async ({ ack, view, client }) => {
           fields: [
             {
               type: "mrkdwn",
-              text: `*Product:*\n${view.state.values.product.product_select_action.selected_option.text.text}`,
+              text: `:rocket: *Product:*\n${view.state.values.product.product_select_action.selected_option.text.text}`,
             },
             {
               type: "mrkdwn",
-              text: `*Project:*\n${view.state.values.project.project_select_action.selected_option.text.text}`,
-            },
-          ],
-        },
-        {
-          type: "divider",
-        },
-        {
-          type: "section",
-          fields: [
-            {
-              type: "mrkdwn",
-              text: `*Role:*\n${view.state.values.role.role_select_action.selected_option.text.text}`,
-            },
-            {
-              type: "mrkdwn",
-              text: `*Foundable's name:*\n${view.state.values.foundable.foundable_action.value}`,
+              text: `:rocket: *Project:*\n${view.state.values.project.project_select_action.selected_option.text.text}`,
             },
           ],
         },
@@ -341,11 +325,27 @@ app.view("create_new_product_action", async ({ ack, view, client }) => {
           fields: [
             {
               type: "mrkdwn",
-              text: `*Tag:*\n${view.state.values.tag.tag_action.value}`,
+              text: `:rocket: *Role:*\n${view.state.values.role.role_select_action.selected_option.text.text}`,
             },
             {
               type: "mrkdwn",
-              text: `*URL:*\n<${view.state.values.url.url_action.value}|Open>`,
+              text: `:rocket: *Foundable's name:*\n${view.state.values.foundable.foundable_action.value}`,
+            },
+          ],
+        },
+        {
+          type: "divider",
+        },
+        {
+          type: "section",
+          fields: [
+            {
+              type: "mrkdwn",
+              text: `:rocket:*Tag:*\n${view.state.values.tag.tag_action.value}`,
+            },
+            {
+              type: "mrkdwn",
+              text: `:rocket: *URL:*\n<${view.state.values.url.url_action.value}|:earth_americas: Open>`,
             },
           ],
         },
