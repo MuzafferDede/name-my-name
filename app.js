a17bfa1381bc3166031d9c721610f27def6ba5c9
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-03-28 21:42:17 +0800

    Added

--- app.js
+++ app.js
@@ -111,3 +111,16 @@ app.action("product_name", async ({ body, payload, ack, say }) => {
 
   console.log("⚡️ Bolt app is running!");
 })();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
