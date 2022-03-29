6e84d4543e56b24f5cb4434930e3378cd0a326f2
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-03-30 02:52:03 +0800

    final

--- callbacks/add-new-item.js
+++ callbacks/add-new-item.js
@@ -18,7 +18,7 @@ const callback = async ({ body, ack, client, logger }) => {
           block_id: "product",
           type: "input",
           element: {
-            type: "select",
+            type: "static_select",
             placeholder: {
               type: "plain_text",
               text: "Select a product",
