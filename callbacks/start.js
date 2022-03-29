df8c93b7ea46e7f526957f296b67b4b6a254a1c0
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-03-30 00:48:37 +0800

    final

--- callbacks/start.js
+++ callbacks/start.js
@@ -1,47 +1,50 @@
-const callback = async ({ event, say }) => {
-  await say({
-    type: "home",
-    blocks: [
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: `Hey there <@${event.user}>! Welcome back! How can i help you today?`,
+const callback = async ({ event, client, logger }) => {
+  await client.views.publish({
+    user_id: event.user,
+    view: {
+      type: "home",
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `Hey there <@${event.user}>! Welcome back! How can i help you today?`,
+          },
         },
-      },
-      {
-        type: "actions",
-        elements: [
-          {
-            type: "button",
-            text: {
-              type: "plain_text",
-              text: "Add new foundable",
+        {
+          type: "actions",
+          elements: [
+            {
+              type: "button",
+              text: {
+                type: "plain_text",
+                text: "Add new foundable",
+              },
+              action_id: "add_new_foundable",
             },
-            action_id: "add_new_foundable",
-          },
-          {
-            type: "button",
-            style: "primary",
-            text: {
-              type: "plain_text",
-              text: "Search foundables",
+            {
+              type: "button",
+              style: "primary",
+              text: {
+                type: "plain_text",
+                text: "Search foundables",
+              },
+              action_id: "search_foundable",
             },
-            action_id: "search_foundable",
-          },
-          {
-            type: "button",
-            style: "danger",
-            text: {
-              type: "plain_text",
-              text: "Manage your foundables",
+            {
+              type: "button",
+              style: "danger",
+              text: {
+                type: "plain_text",
+                text: "Manage your foundables",
+              },
+              action_id: "manage_foundable",
             },
-            action_id: "manage_foundable",
-          },
-        ],
-      },
-    ],
-    text: `Hey there <@${event.user}>!`,
+          ],
+        },
+      ],
+      text: `Hey there <@${event.user}>!`,
+    },
   });
 };
 
