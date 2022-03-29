af9ebd5b7461c7ab321bd96d22ea50b71233fe3f
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-03-30 04:04:49 +0800

    final

--- controller.js
+++ controller.js
@@ -16,65 +16,6 @@ const controller = (app) => {
           type: "plain_text",
           text: "Item created",
         },
-        blocks: [
-          {
-            type: "section",
-            fields: [
-              {
-                type: "mrkdwn",
-                text: `*Product:*\n${view.state.values.product.product_select_action.selected_option.text.text}`,
-              },
-              {
-                type: "mrkdwn",
-                text: `*Project:*\n${view.state.values.project.project_select_action.selected_option.text.text}`,
-              },
-            ],
-          },
-          {
-            type: "divider",
-          },
-          {
-            type: "section",
-            fields: [
-              {
-                type: "mrkdwn",
-                text: `*Role:*\n${view.state.values.role.role_select_action.selected_option.text.text}`,
-              },
-              {
-                type: "mrkdwn",
-                text: `*item's name:*\n${view.state.values.item.item_action.value}`,
-              },
-            ],
-          },
-          {
-            type: "divider",
-          },
-          {
-            type: "section",
-            fields: [
-              {
-                type: "mrkdwn",
-                text: `*Tag:*\n${view.state.values.tag.tag_action.value}`,
-              },
-              {
-                type: "mrkdwn",
-                text: `*URL:*\n<${view.state.values.url.url_action.value}|:earth_americas: Open>`,
-              },
-            ],
-          },
-          {
-            type: "divider",
-          },
-          {
-            type: "context",
-            elements: [
-              {
-                type: "mrkdwn",
-                text: `*${view.state.values.product.product_select_action.selected_option.text.text}* / *${view.state.values.project.project_select_action.selected_option.text.text}* / *${view.state.values.role.role_select_action.selected_option.text.text}* / *${view.state.values.item.item_action.value}* / *${view.state.values.tag.tag_action.value}*`,
-              },
-            ],
-          },
-        ],
       },
     });
   });
