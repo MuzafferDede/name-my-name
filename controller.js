8fac9e0182359e0621b5de0602cf2f7caf3d04fd
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-03-30 23:48:22 +0800

    final

--- controller.js
+++ controller.js
@@ -1,60 +1,16 @@
 const start = require("./callbacks/start.js");
 const addNewItem = require("./callbacks/add-new-item.js");
 const newItem = require("./callbacks/view-new-item.js");
+const projects = require("./effects/projects.js");
 
 const controller = (app) => {
   app.event("app_home_opened", start);
 
   app.action("add_new_item", addNewItem);
 
-  app.action(
-    "product_selected",
-    async ({ ack, action, body, client, context, logger }) => {
-      ack();
-
-      const projects = [
-        {
-          text: {
-            type: "plain_text",
-            text: "PGN",
-          },
-          value: "value-0",
-        },
-        {
-          text: {
-            type: "plain_text",
-            text: "PE",
-          },
-          value: "value-1",
-        },
-      ];
-
-      const blocks = body.view.blocks.map((block) => {
-        if (block.block_id === "project") {
-          block.element.options = projects;
-        }
-
-        return block;
-      });
-
-      console.log(blocks);
-
-      const result = await client.views.update({
-        view_id: body.view.id,
-        view: {
-          title: body.view.title,
-          callback_id: body.view.callback_id,
-          submit: body.view.submit,
-          blocks: blocks,
-          type: body.view.type,
-        },
-      });
-
-      logger.info(result);
-    }
-  );
-
   app.view("view_new_item", newItem);
+
+  app.action("product_selected", projects);
 };
 
 module.exports = controller;
---
+++ effects/projects.js
@@ -0,0 +1,45 @@
+const effect = async ({ ack, body, client, logger }) => {
+  ack();
+
+  const projects = [
+    {
+      text: {
+        type: "plain_text",
+        text: "PGN",
+      },
+      value: "value-0",
+    },
+    {
+      text: {
+        type: "plain_text",
+        text: "PE",
+      },
+      value: "value-1",
+    },
+  ];
+
+  const blocks = body.view.blocks.map((block) => {
+    if (block.block_id === "project") {
+      block.element.options = projects;
+    }
+
+    return block;
+  });
+
+  console.log(blocks);
+
+  const result = await client.views.update({
+    view_id: body.view.id,
+    view: {
+      title: body.view.title,
+      callback_id: body.view.callback_id,
+      submit: body.view.submit,
+      blocks: blocks,
+      type: body.view.type,
+    },
+  });
+
+  logger.info(result);
+};
+
+module.exports = effect;
