630700849a4e9a22820965636abad7919074e579
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-04-03 17:22:11 +0800

    final

--- actions/act-product-selected.js
+++ actions/act-product-selected.js
@@ -1,16 +1,26 @@
 const Project = require("../models/project");
 
-const action = async ({ ack, body, action, ...rest }) => {
+const action = async ({ ack, body, action, client, ...rest }) => {
   console.log("update project");
-  await ack({
-    response_action: "update",
+
+  await client.views.update({
+    view_id: body.view.id,
     view: {
-      type: "modal",
-      callback_id: "handleNewItem",
-      title: {
-        type: "plain_text",
-        text: "Hello World",
-      },
+      title: body.view.title,
+      callback_id: body.view.callback_id,
+      submit: body.view.submit,
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "hello World",
+          },
+        },
+        ...body.view.blocks,
+      ],
+      blocks: blocks,
+      type: body.view.type,
     },
   });
 };
