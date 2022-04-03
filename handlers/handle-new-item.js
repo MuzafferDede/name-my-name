511232b950fa685a126ffd615d787a5fd9ad4ee0
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-04-04 01:29:36 +0800

    final

--- handlers/handle-new-item.js
+++ handlers/handle-new-item.js
@@ -31,7 +31,9 @@ const handler = async ({ ack, body, view, logger }) => {
     .populate({ path: "project", select: "name" })
     .populate({ path: "role", select: "name" })
     .populate({ path: "user", select: "slackId" })
-    .execPopulate(async (err, newItem) => {
+    .exec(async (err, newItem) => {
+      console.log(newItem);
+
       await ack({
         response_action: "update",
         view: {
@@ -46,27 +48,11 @@ const handler = async ({ ack, body, view, logger }) => {
               fields: [
                 {
                   type: "mrkdwn",
-                  text: `*Product:*\n${newItem.product.name}`,
+                  text: `*Product:*\n${item.product.name}`,
                 },
                 {
                   type: "mrkdwn",
-                  text: `*Project:*\n${newItem.project.name}`,
-                },
-              ],
-            },
-            {
-              type: "divider",
-            },
-            {
-              type: "section",
-              fields: [
-                {
-                  type: "mrkdwn",
-                  text: `*Role:*\n${newItem.role.name}`,
-                },
-                {
-                  type: "mrkdwn",
-                  text: `*Item's name:*\n${newItem.name}`,
+                  text: `*Project:*\n${item.project.name}`,
                 },
               ],
             },
@@ -78,11 +64,27 @@ const handler = async ({ ack, body, view, logger }) => {
               fields: [
                 {
                   type: "mrkdwn",
-                  text: `*Tag:*\n${newItem.tag}`,
+                  text: `*Role:*\n${item.role.name}`,
                 },
                 {
                   type: "mrkdwn",
-                  text: `*URL:*\n<${newItem.url}|:earth_americas: Open>`,
+                  text: `*Item's name:*\n${item.name}`,
+                },
+              ],
+            },
+            {
+              type: "divider",
+            },
+            {
+              type: "section",
+              fields: [
+                {
+                  type: "mrkdwn",
+                  text: `*Tag:*\n${item.tag}`,
+                },
+                {
+                  type: "mrkdwn",
+                  text: `*URL:*\n<${item.url}|:earth_americas: Open>`,
                 },
               ],
             },
@@ -94,7 +96,7 @@ const handler = async ({ ack, body, view, logger }) => {
               elements: [
                 {
                   type: "mrkdwn",
-                  text: `*Created by:*\n<@${newItem.user.slackId}>`,
+                  text: `*Created by:*\n<@${item.user.slackId}>`,
                 },
               ],
             },
@@ -106,7 +108,7 @@ const handler = async ({ ack, body, view, logger }) => {
               elements: [
                 {
                   type: "mrkdwn",
-                  text: `*Created at:*\n${newItem.createdAt}`,
+                  text: `*Created at:*\n${item.createdAt}`,
                 },
               ],
             },
