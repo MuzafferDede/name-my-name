b0a97f5d1b9f75992a4c39a4f170138ec8a7d508
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-04-03 15:37:34 +0800

    final

--- options/project-selected.js
+++ options/project-selected.js
@@ -1,7 +1,7 @@
 const Project = require("../models/project");
 
-const callback = async ({ ack, payload, body, state, ...rest }) => {
-  console.log(state);
+const callback = async ({ ack, payload, body, ...rest }) => {
+  console.log(payload.view.state.values);
   const projects = await Project.find({
     name: { $regex: payload.value, $options: "i" },
   });
