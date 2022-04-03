48ed228c8695f78e2d77242105977cae49c1c0fc
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-04-03 17:18:32 +0800

    final

--- actions/act-product-selected.js
+++ actions/act-product-selected.js
@@ -1,6 +1,7 @@
 const Project = require("../models/project");
 
 const action = async ({ ack, body, action, ...rest }) => {
+  console.log("update project");
   await ack({
     response_action: "update",
     view: {
