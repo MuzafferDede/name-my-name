62dc4dc0d1c1b33c3bdf3581ab53e44860552736
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-04-03 18:09:50 +0800

    final

--- options/product-options.js
+++ options/product-options.js
@@ -2,7 +2,7 @@ const Product = require("../models/product");
 
 const options = async ({ ack, payload, ...rest }) => {
   const products = await Product.find({
-    name: { $regex: payload.value, $options: "i" },
+    name: { $regex: "^" + payload.value, $options: "i" },
     projects: { $gt: [] },
   });
 
