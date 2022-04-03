bb8c9f9d2eaf70ac067219960b8f15f7a66f08a3
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-04-04 00:33:52 +0800

    final

--- handlers/handle-new-item.js
+++ handlers/handle-new-item.js
@@ -17,101 +17,96 @@ const handler = async ({ ack, body, view, logger }) => {
     user: user._id,
   });
 
-  await item.save(async (err, item) => {
-    const product = await Product.findOne({
-      _id: item.product,
-    }).exec();
+  await item.save();
 
-    const project = await Project.findOne({
-      _id: item.project,
-    }).exec();
-
-    const role = await Role.findOne({
-      _id: item.role,
-    }).exec();
-
-    await ack({
-      response_action: "update",
-      view: {
-        type: "modal",
-        title: {
-          type: "plain_text",
-          text: "Item created",
+  await Item.findOne({ _id: item._id })
+    .populate("product")
+    .populate("project")
+    .populate("role")
+    .populate("user")
+    .exec(async (err, item) => {
+      await ack({
+        response_action: "update",
+        view: {
+          type: "modal",
+          title: {
+            type: "plain_text",
+            text: "Item created",
+          },
+          blocks: [
+            {
+              type: "section",
+              fields: [
+                {
+                  type: "mrkdwn",
+                  text: `*Product:*\n${item.product.name}`,
+                },
+                {
+                  type: "mrkdwn",
+                  text: `*Project:*\n${item.project.name}`,
+                },
+              ],
+            },
+            {
+              type: "divider",
+            },
+            {
+              type: "section",
+              fields: [
+                {
+                  type: "mrkdwn",
+                  text: `*Role:*\n${item.role.name}`,
+                },
+                {
+                  type: "mrkdwn",
+                  text: `*Item's name:*\n${item.name}`,
+                },
+              ],
+            },
+            {
+              type: "divider",
+            },
+            {
+              type: "section",
+              fields: [
+                {
+                  type: "mrkdwn",
+                  text: `*Tag:*\n${item.tag}`,
+                },
+                {
+                  type: "mrkdwn",
+                  text: `*URL:*\n<${item.url}|:earth_americas: Open>`,
+                },
+              ],
+            },
+            {
+              type: "divider",
+            },
+            {
+              type: "context",
+              elements: [
+                {
+                  type: "mrkdwn",
+                  text: `*Created by:*\n<@${item.user.slackId}>`,
+                },
+              ],
+            },
+            {
+              type: "divider",
+            },
+            {
+              type: "context",
+              elements: [
+                {
+                  type: "mrkdwn",
+                  text: `*Created at:*\n${item.createdAt}`,
+                },
+              ],
+            },
+          ],
         },
-        blocks: [
-          {
-            type: "section",
-            fields: [
-              {
-                type: "mrkdwn",
-                text: `*Product:*\n${product.name}`,
-              },
-              {
-                type: "mrkdwn",
-                text: `*Project:*\n${project.name}`,
-              },
-            ],
-          },
-          {
-            type: "divider",
-          },
-          {
-            type: "section",
-            fields: [
-              {
-                type: "mrkdwn",
-                text: `*Role:*\n${role.name}`,
-              },
-              {
-                type: "mrkdwn",
-                text: `*Item's name:*\n${item.name}`,
-              },
-            ],
-          },
-          {
-            type: "divider",
-          },
-          {
-            type: "section",
-            fields: [
-              {
-                type: "mrkdwn",
-                text: `*Tag:*\n${item.tag}`,
-              },
-              {
-                type: "mrkdwn",
-                text: `*URL:*\n<${item.url}|:earth_americas: Open>`,
-              },
-            ],
-          },
-          {
-            type: "divider",
-          },
-          {
-            type: "context",
-            elements: [
-              {
-                type: "mrkdwn",
-                text: `*Created by:*\n<@${user.slackId}>`,
-              },
-            ],
-          },
-          {
-            type: "divider",
-          },
-          {
-            type: "context",
-            elements: [
-              {
-                type: "mrkdwn",
-                text: `*Created at:*\n${item.createdAt}`,
-              },
-            ],
-          },
-        ],
-      },
+      });
     });
-  });
 };
 
 module.exports = handler;
