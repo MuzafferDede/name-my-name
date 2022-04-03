c6aeb92eeb62cb1bf79bd5d1e40991dec725c363
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-04-03 14:16:48 +0800

    final

--- actions/add-new-item.js
+++ actions/add-new-item.js
@@ -76,6 +76,7 @@ const action = async ({ body, ack, client, logger }) => {
         {
           block_id: "project",
           type: "input",
+          dispatch_action: true,
           element: {
             type: "external_select",
             action_id: "projectSelected",
