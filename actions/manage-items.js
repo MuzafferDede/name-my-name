949294f8d8fd726505ccc51c9559fcb1496ff530
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-04-04 03:14:25 +0800

    final

--- actions/manage-items.js
+++ actions/manage-items.js
@@ -53,7 +53,7 @@ const action = async ({ body, ack, client, action, logger, ...rest }) => {
   const items = blocks.length
     ? blocks.reduce((acc, block, index) => {
         if (index % 2 === 0) {
-          return [...acc, block, { type: "divider" }];
+          return [...acc, { type: "divider" }, block];
         }
 
         return [...acc, block];
