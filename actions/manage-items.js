4ad63d8915859d54ee0ed2dced6f9940ccde43d1
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-04-04 00:50:08 +0800

    final

--- actions/manage-items.js
+++ actions/manage-items.js
@@ -3,13 +3,13 @@ const User = require("../models/user");
 const action = async ({ body, ack, client, logger }) => {
   await ack();
 
-  const items = await User.find({
+  const user = await User.find({
     slackId: body.user.id,
   })
     .populate("items")
     .exec();
 
-  logger.info(items);
+  logger.info(user.items);
 
   const result = await client.views.open({
     trigger_id: body.trigger_id,
