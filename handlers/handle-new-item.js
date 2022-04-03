a696019631a1fcc81d0fd84927bd6cca65236234
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-04-03 19:34:51 +0800

    final

--- handlers/handle-new-item.js
+++ handlers/handle-new-item.js
@@ -14,93 +14,95 @@ const handler = async ({ ack, body, view, logger }) => {
     user: user._id,
   });
 
-  await Promise.resolve(item.save);
+  await item.save((err,item) => {
+    const newItem = await Item.findOne({ _id: item._id }).exec();
 
-  const newItem = await Item.findOne({ _id: item._id }).exec();
-
-  console.log(newItem);
-
-  await ack({
-    response_action: "update",
-    view: {
-      type: "modal",
-      title: {
-        type: "plain_text",
-        text: "Item created",
+    console.log(newItem);
+  
+    await ack({
+      response_action: "update",
+      view: {
+        type: "modal",
+        title: {
+          type: "plain_text",
+          text: "Item created",
+        },
+        blocks: [
+          {
+            type: "section",
+            fields: [
+              {
+                type: "mrkdwn",
+                text: `*Product:*\n${newItem.product.name}`,
+              },
+              {
+                type: "mrkdwn",
+                text: `*Project:*\n${newItem.project.name}`,
+              },
+            ],
+          },
+          {
+            type: "divider",
+          },
+          {
+            type: "section",
+            fields: [
+              {
+                type: "mrkdwn",
+                text: `*Role:*\n${newItem.role.name}`,
+              },
+              {
+                type: "mrkdwn",
+                text: `*item's name:*\n${newItem.name}`,
+              },
+            ],
+          },
+          {
+            type: "divider",
+          },
+          {
+            type: "section",
+            fields: [
+              {
+                type: "mrkdwn",
+                text: `*Tag:*\n${newItem.tag}`,
+              },
+              {
+                type: "mrkdwn",
+                text: `*URL:*\n<${newItem.url}|:earth_americas: Open>`,
+              },
+            ],
+          },
+          {
+            type: "divider",
+          },
+          {
+            type: "context",
+            elements: [
+              {
+                type: "mrkdwn",
+                text: `*Created by:*\n<@${newItem.user.slackId}>`,
+              },
+            ],
+          },
+          {
+            type: "divider",
+          },
+          {
+            type: "context",
+            elements: [
+              {
+                type: "mrkdwn",
+                text: `*Created at:*\n${newItem.createdAt}`,
+              },
+            ],
+          },
+        ],
       },
-      blocks: [
-        {
-          type: "section",
-          fields: [
-            {
-              type: "mrkdwn",
-              text: `*Product:*\n${newItem.product.name}`,
-            },
-            {
-              type: "mrkdwn",
-              text: `*Project:*\n${newItem.project.name}`,
-            },
-          ],
-        },
-        {
-          type: "divider",
-        },
-        {
-          type: "section",
-          fields: [
-            {
-              type: "mrkdwn",
-              text: `*Role:*\n${newItem.role.name}`,
-            },
-            {
-              type: "mrkdwn",
-              text: `*item's name:*\n${newItem.name}`,
-            },
-          ],
-        },
-        {
-          type: "divider",
-        },
-        {
-          type: "section",
-          fields: [
-            {
-              type: "mrkdwn",
-              text: `*Tag:*\n${newItem.tag}`,
-            },
-            {
-              type: "mrkdwn",
-              text: `*URL:*\n<${newItem.url}|:earth_americas: Open>`,
-            },
-          ],
-        },
-        {
-          type: "divider",
-        },
-        {
-          type: "context",
-          elements: [
-            {
-              type: "mrkdwn",
-              text: `*Created by:*\n<@${newItem.user.slackId}>`,
-            },
-          ],
-        },
-        {
-          type: "divider",
-        },
-        {
-          type: "context",
-          elements: [
-            {
-              type: "mrkdwn",
-              text: `*Created at:*\n${newItem.createdAt}`,
-            },
-          ],
-        },
-      ],
-    },
+    });
   });
+
+
 };
 
 module.exports = handler;
