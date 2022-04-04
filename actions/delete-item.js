51e05222fe01926e83acc942c7325d70d3e8ab9b
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-04-04 17:36:39 +0800

    final

--- actions/delete-item.js
+++ actions/delete-item.js
@@ -1,11 +1,16 @@
 const User = require("../models/user");
+const Item = require("../models/item");
 
 const action = async ({ body, ack, client, action, logger, ...rest }) => {
-  const user = await User.findOne({
-    slackId: body.user.id,
-  })
-    .populate("items")
-    .exec();
+  await Item.findOneAndRemove({ _id: action.value });
+
+  const user = await User.findOne({ slackId: body.user.id });
+
+  user.items = user.items.filter(
+    (item) => item._id.toString() !== action.value
+  );
+
+  await user.save();
 
   await ack();
 
@@ -53,16 +58,13 @@ const action = async ({ body, ack, client, action, logger, ...rest }) => {
         },
       ];
 
-  await client.views.open({
-    trigger_id: body.trigger_id,
+  await client.views.update({
+    view_id: body.view.id,
     view: {
-      callback_id: "handleManageItems",
-      type: "modal",
-      title: {
-        type: "plain_text",
-        text: "Your items",
-      },
+      title: body.view.title,
+      callback_id: body.view.callback_id,
       blocks: items,
+      type: body.view.type,
     },
   });
 };
