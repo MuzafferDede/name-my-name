cf55c04f1597bef390697f1bad3d2cb86088846c
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-04-04 16:17:02 +0800

    final

--- options/search-result-options.js
+++ options/search-result-options.js
@@ -34,7 +34,7 @@ const options = async ({ ack, payload, ...rest }) => {
   });
 };
 
-const truncate = (str, length = 30) => {
+const truncate = (str, length = 75) => {
   if (str.length > length) {
     return str.substring(0, length) + "...";
   }
