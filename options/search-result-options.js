088453dd0a6caee0ba4eef902c736d210a91934e
戴蒙Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2022-04-04 15:12:35 +0800

    final

--- options/search-result-options.js
+++ options/search-result-options.js
@@ -1,16 +1,10 @@
 const Product = require("../models/product");
 
 const options = async ({ ack, payload, ...rest }) => {
-  const products = await Product.find({
-    $or: [
-      {
-        name: { $regex: new RegExp(payload.value), $options: "i" },
-      },
-      {
-        tag: { $regex: new RegExp(payload.value), $options: "i" },
-      },
-    ],
-  });
+  const products = await Product.find().or([
+    { name: { $regex: new RegExp(payload.value), $options: "i" } },
+    { tag: { $regex: new RegExp(payload.value), $options: "i" } },
+  ]);
 
   const productList = products.map((product) => {
     return {
